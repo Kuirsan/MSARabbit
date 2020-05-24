80562371df69493e9c9bd501646a26aa6d63402f
Kirill Pavlov <ksmersh@gmail.com>
2020-05-25 00:42:29 +0300

    Adding code to Domain.Core

---
+++ MicroRabbit/MSARabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using MSARabbit.Domain.Core.Commands;
+using MSARabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSARabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>() 
+            where T : Event
+            where TH : IEventHandler;
+    }
+}
---
+++ MicroRabbit/MSARabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using MSARabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSARabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent:Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
