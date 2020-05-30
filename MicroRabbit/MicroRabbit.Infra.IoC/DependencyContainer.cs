b0eef500f4be029b860b7cec9e570c881d6ec246
Kirill Pavlov <ksmersh@gmail.com>
2020-05-30 21:46:39 +0300

    Infrra for IoC

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using MSARabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
