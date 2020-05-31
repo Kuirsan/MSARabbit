86fe5b2f0aa4ed6d5e88a1d82ec4bca19d3f1bc6
Kirill Pavlov <ksmersh@gmail.com>
2020-05-31 23:00:43 +0300

    Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    class BankingDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDbContext>();
+            optionsBuilder.UseSqlServer("Server = DESKTOP-198JHES\\SQLEXPRESS; Database = BankingDB; Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new BankingDbContext(optionsBuilder.Options);
+        }
+    }
+}
